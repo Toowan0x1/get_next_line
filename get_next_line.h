3492fba91e417d8438c765b39efa740aa2915c47
Omar El Houmadi <50034094+Toowan0x1@users.noreply.github.com>
2024-01-12 13:05:53 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -11,10 +11,12 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
+
 # define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
+# define BUFFER_SIZE 10
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
